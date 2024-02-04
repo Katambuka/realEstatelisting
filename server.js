940f4e345904980b2968894e797370bff0cf31e4
Katambuka <kan22001@byui.edu>
2024-02-04 09:56:20 +0400

    swagger-error fixing2

--- server.js
+++ server.js
@@ -1,21 +1,13 @@
 const express = require('express');
 const mongodb = require('./data/database');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 
 const port = process.env.PORT || 8000;
 
 app.use(bodyParser.json());
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
-  );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  next();
-});
+app.use(cors());
 
 app.use('/', require('./routes'));
 
